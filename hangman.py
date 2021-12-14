f43bdf3163e45f9debf8b750364764fc543c19ac
moonjunehyeok <uriage1108@naver.com>
2021-12-14 15:08:55 +0900

    fix

--- hangman.py
+++ hangman.py
@@ -342,7 +342,7 @@ def mode():
                 if width >= 640 and width <=850:
                     if height >= 180 and height <= 240:
                         return 0
-                    elif height >= 400 and height <= 480:
+                    elif height >= 400 and height <= 460:
                         return 1
 
         win.fill(WHITE)
